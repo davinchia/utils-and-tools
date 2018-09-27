0c7ab2bfafff4c38820b57a1b46fc6c0b3359a63
Davin Chia <dchia@liveramp.com>
2018-09-27 14:16:05 +0100

    Set up etcd. (#1)
    * Create etcd namespace. Create ssd storageclass type. Create etcd-stateful set along with required etcd headless service.
    
    * Initial script to save all kube secrets to vault.
    
    * Add comments.
    
    * Add newline to EOF.
    
    * Add newline to eof.

---
+++ scripts/save-secrets.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# This script writes all secrets in a given namespace to vault
+# with the path secret/yamls/$team/$namespace/$secret-name
+# 
+# Before being saved, the secrets are saved to a temp directory for sanitation.
+# This temp directory is deleted when the script finishes.
+
+export tmp_file=/tmp/secrets
+export team=pixel
+export namespace=reporting
+
+if [ -d "$tmp_file" ]; then
+  echo "Clearing temp secrets directory at $tmp_file"
+  rm -r $tmp_file
+fi
+
+echo "Creating temp secrets directory.."
+mkdir -p $tmp_file
+
+echo "Downloading secrets.."
+kubectl get secrets -n $namespace | awk -F " " '{print $1}' | grep -v NAME \
+| xargs -I % sh -c 'kubectl get secret -n $namespace "$1" -o json > "$tmp_file/$1"' -- %
+
+echo "Writing secrets to vault.."
+for filename in $tmp_file/*; do
+    # default-token is the authentication token kube uses for the default service account,
+    # skip it as it is kube cluster specific.
+    if [[ ${filename} != *"default-token"* ]];then
+    b=$(basename $filename)
+    echo $b
+    # Make sure to strip potentially confusing metadata from the secret.
+    cat $filename | jq 'del(.metadata.annotations, .metadata.creationTimestamp, .metadata.resourceVersion, .metadata.uid, .metadata.selfLink, .metadata.namespace)' \
+    | vault write secret/yamls/$team/$namespace/$b -
+    fi
+done
+echo "Done writing secrets"
+
+echo "Removing temp secrets directory.."
+rm -r $tmp_file
